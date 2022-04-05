a81609dc993d96515ecd2dd9983a0599442657f3
Ashley <ashley.shultz@canoo.com>
2022-04-05 16:20:13 -0700

    get dist working

--- python_projects/acme_corp/app1/src/acme_corp/app1/app1_module.py
+++ python_projects/acme_corp/app1/src/acme_corp/app1/app1_module.py
@@ -1,5 +1,5 @@
-from acme_corp.lib.lib1_module import lib1_func
+from acme_corp.lib1.lib1_module import lib1_func
 
 def app1_func():
-    print('app1_func!')
+    print('lib1_func app1_func!')
     lib1_func()
