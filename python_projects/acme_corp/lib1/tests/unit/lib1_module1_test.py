5508858099333921347ed3e5789426b9f403c9e1
Ashley <ashley.shultz@canoo.com>
2022-04-05 17:10:39 -0700

    add some tests

---
+++ python_projects/acme_corp/lib1/tests/unit/lib1_module1_test.py
@@ -0,0 +1,6 @@
+from acme_corp.lib1.lib1_module1 import lib1_func1
+
+
+def test_lib1_func1():
+    lib1_func1()
+    assert True
---
+++ python_projects/acme_corp/lib1/tests/unit/lib1_module2_test.py
@@ -0,0 +1,6 @@
+from acme_corp.lib1.lib1_module2 import lib1_func3
+
+
+def test_lib1_func1():
+    lib1_func3()
+    assert True
